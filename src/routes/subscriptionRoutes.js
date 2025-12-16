5c828488432a94e8c6e24ad69dd18ac02763974c
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-12-16 08:33:30 -0300

    fix

--- src/routes/subscriptionRoutes.js
+++ src/routes/subscriptionRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+// Protege a rota para garantir que temos o req.user.id
 router.use(authMiddleware);
 
 router.post('/verify', subscriptionController.verifyPurchase);
